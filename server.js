ad4f1445b408643b93a3e84b8f82325604f98adb
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-12-20 18:23:51 -0500

    Move db connection code to server.js

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
-//Require NPM packages, port, and routes
+//Require NPM packages
 const express = require('express'),
-  app = express(),
-  port = 3000,
-  routes = require('./routes/htmlRoutes');
+  exphbs = require('express-handlebars'),
+  mongoose = require('mongoose');
 
-//Enable Express middleware
+//Set MongoDB
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/newsScraper';
+
+//Connect to DB
+mongoose.connect(
+  MONGODB_URI,
+  { useNewUrlParser: true }
+);
+
+//Set express instance, port and routes
+const app = express(),
+  port = process.env.PORT || 3000,
+  htmlRoutes = require('./routes/htmlRoutes'),
+  apiRoutes = require('./routes/apiRoutes');
+
+//Enable middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Set handlebars as app template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//Set static content directory
 app.use(express.static('public'));
-//app.use(routes);
+
+//Set routes
+app.use(htmlRoutes);
+app.use(apiRoutes);
 
 //Start the server
 app.listen(port, () => {
