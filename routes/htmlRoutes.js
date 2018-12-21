597ad2dda3206ca60dfa744eca02a2462b186846
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-12-21 12:02:49 -0500

    Add 'catch all' route for 404 page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,10 +8,12 @@ const router = express.Router();
 router.get('/', (req, res) => {
   db.Article.find({})
     .then(docs => res.status(200).render('index', { article: docs }))
-    //res.render('index', docs);
     .catch(err => res.status(500).json(err));
   //res.render('404');
 });
+
+//"Catch All" route
+router.get('*', (req, res) => res.render('404'));
 
 //Export router
 module.exports = router;
