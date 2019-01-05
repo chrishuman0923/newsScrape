b873e4c148c4f5ea1b2185ecc88e2a8e35f5df08
chuman <42072991+chrishuman0923@users.noreply.github.com>
2019-01-05 14:01:29 -0500

    Add image to database insert

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,43 +8,62 @@ const express = require('express'),
 //Create router
 const router = express.Router();
 
+//Get articles
 router.get('/scrape', (req, res) => {
-  axios.get('http://www.northwestgeorgianews.com/rome/news/').then(resp => {
-    const $ = cheerio.load(resp.data);
+  axios
+    .get('http://www.northwestgeorgianews.com/rome/news/')
+    .then(resp => {
+      const $ = cheerio.load(resp.data);
 
-    $('.card-body').each(function() {
-      let result = {};
+      $('.card-container').each(function() {
+        let result = {};
 
-      result.title = $(this)
-        .find('.tnt-headline')
-        .find('a')
-        .text();
-
-      result.link =
-        'http://www.northwestgeorgianews.com' +
-        $(this)
+        result.title = $(this)
           .find('.tnt-headline')
           .find('a')
-          .attr('href');
+          .text();
 
-      result.articleDate = moment(
-        $(this)
+        result.link =
+          'http://www.northwestgeorgianews.com' +
+          $(this)
+            .find('.tnt-headline')
+            .find('a')
+            .attr('href');
+
+        result.date = $(this)
           .find('.card-date')
           .find('time')
-          .attr('datetime')
-      ).format('MM/DD/YY, h:mm a');
+          .attr('datetime');
 
-      result.summary = $(this)
-        .find('.tnt-summary')
-        .text();
+        result.displayDate = moment(
+          $(this)
+            .find('.card-date')
+            .find('time')
+            .attr('datetime')
+        ).format('MM/DD/YYYY hh:mm a');
 
-      db.Article.create(result)
-        .then(dbArticle => console.log(dbArticle))
-        .catch(err => console.error(err));
-    });
+        result.summary = $(this)
+          .find('.tnt-summary')
+          .text();
 
-    res.send('Scrape Complete');
-  });
+        result.img = $(this)
+          .find('.image')
+          .find('img')
+          .attr('data-src');
+
+        result.imgAlt = $(this)
+          .find('.image')
+          .find('img')
+          .attr('alt');
+
+        db.Article.create(result)
+          .then(dbArticle => console.log(dbArticle))
+          .catch(err => console.error(err));
+      });
+
+      res.status(200).send('Complete');
+    })
+    .catch(err => res.status(500).send(err));
 });
 
 module.exports = router;
