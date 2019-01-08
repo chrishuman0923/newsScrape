2a64d93684629716d29748728cc35a2a72fce8f8
chuman <42072991+chrishuman0923@users.noreply.github.com>
2019-01-07 21:54:36 -0500

    Add post to form button

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
-  const $scrapeBtn = $('#scrapeBtn');
+  const $scrapeBtn = $('#scrapeBtn'),
+    $addNoteBtn = $('.addNoteBtn');
 
   $scrapeBtn.on('click', () => {
     $.get('/scrape', resp => {
@@ -7,5 +8,22 @@ $(document).ready(() => {
         window.location.href = '/';
       }
     });
+  });
+
+  $addNoteBtn.on('click', function() {
+    //prevent the form from refreshing the page
+    event.preventDefault();
+
+    //Get article ID and note contents
+    let articleID = $(this).attr('data-id'),
+      noteBody = $(`#${articleID}`).val();
+
+    if (noteBody) {
+      $.post(`/articles/${articleID}`, { body: noteBody }, () => {
+        window.location.href = '/';
+      });
+    } else {
+      alert('Please enter a note.');
+    }
   });
 });
